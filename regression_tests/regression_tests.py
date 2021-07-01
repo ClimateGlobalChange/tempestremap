024d107dbbbebc653a6ed1d2581d4028ca4b7e23
Rajeev Jain <jain@homes01.cels.anl.gov>
2021-07-01 01:58:07 -0500

    o Add argument for number of procs and fix bugs

--- regression_tests/regression_tests.py
+++ regression_tests/regression_tests.py
@@ -25,55 +25,40 @@ def run_command(cmd):
 
     # TODO: dump each command line output to file(s)
     res = []
-    # use the communicate function to fetch the output
-    output = str(temp.communicate())
-
-    # # splitting the output to parse ine by line
-    # output = output.split("\n")
-
-    # output = output[0].split('\\')
-
-    # # a variable to store the output
-    # res = []
-
-    # iterate through the output line by line
-    for line in output:
-        res.append(line)
-
-    return res
-
-
-
-# a function to run a command and parse result (Apply Offline Map)
-def run_command_results(cmd):
-
-    # using the Popen function to execute the command 
-    mycmd =""
-    for j in range(len(cmd)):
-        mycmd+=cmd[j]+" "
-
-    cmd = mycmd     
-    print("cmd running:", cmd,)
-    temp = subprocess.Popen(cmd, shell=True, stdout = subprocess.PIPE)
+    success = True
 
     # use the communicate function to fetch the output
-    output = str(temp.communicate())
+    output, errs = temp.communicate()
+   
+   # if the output is empty - executables not found then return False   
+    if output == b'':
+       success = False
 
-    # splitting the output to parse ine by line
+    # splitting the output to parse line by line
+    output = str(output)
     output = output.split("\n")
 
-    output = output[0].split('\\')
+    output = output[0].split('\\n')
 
     # a variable to store the output
     res = []
 
     # iterate line by line
     for line in output:
+      #   print (line)
+      #  if we find the word EXCEPTION in the output report Failure
+        if "EXCEPTION" in line:
+           print("Error running ", cmd, "\n\n", line)
+           success = False 
         res.append(line)
+
+    return res, success
+
+def create_baseline_results(id, res):
 
     opts = []
 
-    filename = "./data/regression_results.txt"
+    filename = "./baseline/regression_results.txt"
     if not os.path.exists(os.path.dirname(filename)):
         try:
             os.makedirs(os.path.dirname(filename))
@@ -82,34 +67,35 @@ def run_command_results(cmd):
                 raise
 
     with open(filename, "a") as f:
-        f.write("\nRegression Log\n")
+         for i in range(1, len(res) - 1):
+               line = res[i]
+               # if "--in_data <string>" in line or "--map <string> " in line:
+               #    print(line)
+               #    # f.write(line)
+               #    # f.write("\n")
 
-        print("\nTest Result\n")
-
-        for i in range(1, len(res) - 1):
-            line = res[i]
-            if "--in_data <string>" in line or "--map <string> " in line:
-                print(line)
-                f.write(line)
-                f.write("\n")
-            a = line.find("....")
-            if a != -1:
-                opt = line.partition("....")[2].split()[2]
-        # ['Source', 'Mass:', '2.513274122871826e+01', 'Min', '1.0027367453e+00', 'Max', '2.9972632547e+00']
-                opts.append(opt)
-                # print(line.partition("....")[2].split()[0], " ", opt)
+               #  look for the pattern below to find mass values
+               a = line.find("....")
+               if a != -1:
+                  opt = line.partition("....")[2].split()[2]
+                  # parse mass value for source and target both preceeded by .... eg. below value is 2.513274122871826e+01
+                  # ['Source', 'Mass:', '2.513274122871826e+01', 'Min', '1.0027367453e+00', 'Max', '2.9972632547e+00']
+                  opts.append(opt)
+                  # print(line.partition("....")[2].split()[0], " ", opt)
 
 
-        diff = float(opts[0]) - float(opts[1])
-        percentage_diff = ( diff * 100 )/float(opts[0])
-        f.write("\n")
-        text = "Percentage diff between source and target is " + str(percentage_diff)
-        f.write("\n")
-        f.write(text)
-        print(text)
+         diff = float(opts[0]) - float(opts[1])
+         percentage_diff = ( diff * 100 )/float(opts[0])
+         # f.write("\n")
+         text = "CaseID: " + str(id) + ": src val, target val, err " 
+         f.write("\n")
+         f.write(text)
+         f.write("\n")
+         vals =  str(id) + " " + str(opts[0]) + " " + str(opts[1]) + " " + str(percentage_diff)
+         f.write(vals)
+         f.write("\n")
 
-    return res
-
+         print(vals)   
 
 def generate_cs_mesh(res, filename):
     command = []
@@ -243,12 +229,21 @@ def generate_mesh(meshstr):
 
     return command, filename
 
+def check_error(success):
+    if False in success:
+       print("exiting..")
+       print("Usage: \npython regression_tests.py <location of executables>  <number of procs>\n    Default <location of executables> is ../bin/ and <number of procs> is 2\n")
+
+       exit()   
 
 if __name__ == '__main__':
-    # if len(sys.argv) < 2:
-    #     print("Usage: \npython regression_tests.py <location of executables>\n\n")
-    #     exit()
-    # path = sys.argv[1] + "/"
+    procs = 2
+    print("Usage: \npython regression_tests.py <location of executables>  <number of procs>\n    Default <location of executables> is ../bin/ and <number of procs> is 2\n")
+    if len(sys.argv) >= 2:
+       bin_path = sys.argv[1] + "/"
+
+    if len(sys.argv) == 3:
+        procs= int (sys.argv[2])
 
     # Run a pipeline
     # read inputs
@@ -285,7 +280,7 @@ if __name__ == '__main__':
         method = "exact"
 
         # call overlap mesh
-            # name overlap mesh as in+outfile.g
+        # name overlap mesh as in+outfile.g
         overlapmesh = in_file[:len(in_file)-2]+"-"+out_file
 
         cmd = generate_overlap_mesh(in_file, out_file, method, overlapmesh)
@@ -318,20 +313,20 @@ if __name__ == '__main__':
 
 # Generate Test Data
         test = str(tm.loc[i].at["test"])
-        out_td="test"
+        out_td_src="test"
         #srcmesh
-        out_td+=srcmesh_str.upper()
-        out_td+="-F"+test+"-O"+order+".nc"
+        out_td_src+=srcmesh_str.upper()
+        out_td_src+="-F"+test+"-O"+order+".nc"
 
-        cmd = generate_test_data(in_file, test, out_td)
+        cmd = generate_test_data(in_file, test, out_td_src)
         generate_test_cmds.append(cmd)
 
-        out_td="test"
-        #srcmesh
-        out_td+=tgtmesh_str.upper()
-        out_td+="-F"+test+"-O"+order+".nc"
-        cmd = generate_test_data(out_file, test, out_td)
-        generate_test_cmds.append(cmd)
+      #   out_td_tgt="test"
+      #   #tgtmesh
+      #   out_td_tgt+=tgtmesh_str.upper()
+      #   out_td_tgt+="-F"+test+"-O"+order+".nc"
+      #   cmd = generate_test_data(out_file, test, out_td_tgt)
+      #   generate_test_cmds.append(cmd)
 
 # Apply Offline Map
         variablename = "Psi"
@@ -339,26 +334,74 @@ if __name__ == '__main__':
         out_test+=srcmesh_str.upper()
         out_test+="-F"+test+"-O"+order+".nc"
 
-        cmd = apply_offline_map(ofmap_out, out_td, variablename, out_test)          
+        cmd = apply_offline_map(ofmap_out, out_td_src, variablename, out_test)          
         a_offmap_cmds.append(cmd)  
 
-    print(mesh_cmds)
-    print(generate_test_cmds)
-    print(overlap_test_cmds)
-    print(g_offmap_cmds)
-    print(a_offmap_cmds)
+    
 
 # Running multiple processes in parallel and in sequence
-    total_cmds = len(mesh_cmds) + len(overlap_test_cmds) + len(g_offmap_cmds) + len(a_offmap_cmds)
-    pool = Pool(processes=2)
-    pool.map(run_command, mesh_cmds)
-    pool.map(run_command, generate_test_cmds)
-    pool.map(run_command, overlap_test_cmds)
-    pool.map(run_command, g_offmap_cmds)
-    results = pool.map(run_command_results, a_offmap_cmds)
-    print(results)
+    pool = Pool(processes=procs)
+   #  
+   # get unique commands to run, convert list to set and back
+    mesh_cmds = [list(x) for x in set(tuple(x) for x in mesh_cmds)] 
+    generate_test_cmds = [list(x) for x in set(tuple(x) for x in generate_test_cmds)] 
+    overlap_test_cmds = [list(x) for x in set(tuple(x) for x in overlap_test_cmds)] 
+    g_offmap_cmds = [list(x) for x in set(tuple(x) for x in g_offmap_cmds)] 
+    a_offmap_cmds = [list(x) for x in set(tuple(x) for x in a_offmap_cmds)]
+   # 
 
+    success = []
+    results = []
+    count = 0
+   #  print(mesh_cmds)
+    for result, ss in pool.map(run_command, mesh_cmds):
+       results.append(result)
+       success.append(ss)
+    check_error(success)
+   #  
+    success = []
+    results = []
+   #  print(generate_test_cmds) 
+    for result, ss in pool.map(run_command, generate_test_cmds):
+       results.append(result)
+       success.append(ss)    
+    check_error(success)
+   #
+    success = []
+    results = [] 
+   #  print(overlap_test_cmds)
+    for result, ss in pool.map(run_command, overlap_test_cmds):
+       results.append(result)
+       success.append(ss)    
+    check_error(success)
+   # 
+    success = []
+    results = []    
+   #  print(g_offmap_cmds)
+    for result, ss in pool.map(run_command, g_offmap_cmds):
+       results.append(result)
+       success.append(ss)    
+    check_error(success)
+   # 
+    success = []
+    results = []    
+   #  print(a_offmap_cmds)
+    for result, ss in pool.map(run_command, a_offmap_cmds):
+       results.append(result)
+       success.append(ss)       
+    check_error(success)
+   #  print(results)
     pool.close()
     pool.join()
+
+    # create baseline results
+    count = 0
+    for i in range(len(tm.values)):
+      id = tm.loc[i].at["id"]
+      create_baseline_results(id, results[count])
+      count+=1
+
+    # TODO: compare new against baselines
+
 
 
